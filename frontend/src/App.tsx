f8efb9fd84f721bacae84d2c85c699e3520d7007
jbrofee <jack@jackbrophy.dev>
2025-09-25 14:24:13 +0100

    Configured Vite to build into overlay directory to be served by Fastify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
 import "./App.css";
 
 function App() {
+  const wsConnection = new WebSocket("ws://localhost:3001/websocket");
+  wsConnection.onopen = () => {
+    console.log("Connected to WebSocket server");
+  };
+
+  wsConnection.onmessage = (message) => {
+    console.log("Message recevied: " + message);
+  };
+
+  wsConnection.onclose = () => {
+    console.log("WebSocket connection closed");
+  };
+
+  function sendWsMessage() {
+    wsConnection.send("Hello!");
+  }
+
   return (
     <>
       <div className="text-2xl">Testing Tailwind</div>
+      <button onClick={sendWsMessage}>Send test message</button>
     </>
   );
 }
